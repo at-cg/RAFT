07d3b051b04ae192ffd9e1c1aaaa0228da122ca0
MehakBindra <mhk0397@gmail.com>
2023-01-09 12:00:50 +0530

    add prefix to bed

--- chop.hpp
+++ chop.hpp
@@ -128,7 +128,7 @@ void break_long_reads(const char *readfilename, const char *paffilename, const a
 {
 
     std::ofstream reads_final("output_reads.fasta");
-    std::ofstream bed_fragmented(param.outputfilename + "fragmentation.bed");
+    std::ofstream bed_fragmented(param.outputfilename + ".fragmentation.bed");
 
     int n_read;
     int64_t n_aln = 0;
