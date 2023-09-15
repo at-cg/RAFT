4961dbd0214b78eaf3931f78cc270653f512caa2
Chirag Jain <cjain7@gatech.edu>
2023-09-15 04:55:15 -0700

    changes to countT2T.sh

--- bash_scripts/countT2T.sh
+++ bash_scripts/countT2T.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+# https://anaconda.org/bioconda/tidk
 TIDK=/path/to/tidk
 SAMTOOLS=/path/to/samtools
 COUNTER=~/bash_scripts/T2T-count.pl
 
 SEQ=/path/to/concatenated/assembly/contigs/output.bp.hap1hap2.p_ctg.fa
 
-OUTDIR=/path/to/output/directory
 PREFIX=output
 TELOMERE=TTAGGG
 
@@ -18,10 +18,12 @@ if [ ! -d t2tCounts/ ]; then
 fi
 
 #input 10000 for window parameter is default
-$TIDK search --dir ./t2tCounts/ --fasta $SEQ --output $OUTPUT --string $TELOMERE --window 10000
+$TIDK --version
+$TIDK search --dir ./t2tCounts/ --extension tsv --output $PREFIX --string $TELOMERE --window 10000 $SEQ
+sed 's/\t/,/g' ./t2tCounts/${PREFIX}_telomeric_repeat_windows.tsv > ./t2tCounts/${PREFIX}_telomeric_repeat_windows.csv
 
 #100000 is the length of prefix and suffix of contig where $TELOMERE is searched; 100 is the lower bound cutoff;
-perl $COUNTER ./t2tCounts/search_telomeric_repeat_windows.csv output.bp.hap1hap2.p_ctg.fa.fai 100000 100 > ./t2tCounts/t2tSearch.out
+perl $COUNTER ./t2tCounts/${PREFIX}_telomeric_repeat_windows.csv ${SEQ}.fai 100000 100 > ./t2tCounts/t2tSearch.out
 
 #list out all contigs which satisfy telomere count criterion
 awk '$3>100 && $4>100' ./t2tCounts/t2tSearch.out > ./t2tCounts/search.list
